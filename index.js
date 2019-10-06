cf4c1fe6bbddf9887e7d6d7fd8821c7573654c02
Taewan-P <swimingtw@gmail.com>
2019-10-06 15:40:14 +0900

    Fix ES6 indentation and semi colons
    Did not fix  the indentation that are in tabs.

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const port = 3000;
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 3000
+const app = express()
 
-
-/*middle ware*/
-app.use(express.static(__dirname + '/public'));
-app.use(function (req, res, next){
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Methods", 'GET');//POST, PUT, DELETE 는 구현하지 않음.
-    res.header("Access-Control-Allow-Headers", "content-type");
-    next();
+/* middle ware */
+app.use(express.static(__dirname + '/public'))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET') // POST, PUT, DELETE 는 구현하지 않음.
+    res.header('Access-Control-Allow-Headers', 'content-type')
+    next()
 })
 
+/* body-parser */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-/*body-parser*/
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true}));
-
-
-/*router*/
-
-app.use(['/semester/week/giksa', '/semester/weekend/giksa', 'vacation/week/giksa','vacation/weekend/giksa'], require('./router/giksa'));
-app.use(['/semester/week/shuttlecock', '/semester/weekend/shuttlecock', 'vacation/week/shuttlecock', 'vacation/weekend/shuttlecock'], require('./router/shuttlecock'));
-app.use(['/semester/week/subway', '/semester/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'));
-app.use(['/semester/week/yesulin', '/semester/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'));
+/* router */
+app.use(['/semester/week/giksa', '/semester/weekend/giksa', 'vacation/week/giksa','vacation/weekend/giksa'], require('./router/giksa'))
+app.use(['/semester/week/shuttlecock', '/semester/weekend/shuttlecock', 'vacation/week/shuttlecock', 'vacation/weekend/shuttlecock'], require('./router/shuttlecock'))
+app.use(['/semester/week/subway', '/semester/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
+app.use(['/semester/week/yesulin', '/semester/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
 app.use((req, res, next) => {
-    next(createError(404));
-});
+    next(createError(404))
+})
 
 app.use((err, req, res, next) => {
         res.status(404)
         res.json({ errorcode: '404' })
-});
+})
 
-/*server*/
-app.listen(port, function(){
-    console.log('listening on port : ' + port);
-});
+/* server */
+app.listen(port, function () {
+    console.log('listening on port : ' + port)
+})
--- router/giksa.js
+++ router/giksa.js
@@ -1,47 +1,44 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
-const fs = require('fs');
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+const fs = require('fs')
 
-
-function urlParse(urlstr) {
-    var urlArr = urlstr.split('/');
-    return urlArr;
+function urlParse (urlstr) {
+    var urlArr = urlstr.split('/')
+    return urlArr
 }
 
 router.get('/', (req, res) => {
-    //접근 url 은 항상 "학기,방학/주중,주말/정류장 종류 의 형태로 들어옴."
-    var urlArr = urlParse(req.originalUrl);
+    // 접근 url 은 항상 "학기,방학/주중,주말/정류장 종류 의 형태로 들어옴."
+    var urlArr = urlParse(req.originalUrl)
 
     switch (urlArr[1]) {
-        case 'semester': //학기중
-            if(urlArr[2] == 'week'){ //주중
-                var jsonPath = path.join(__dirname, '..', 'timetable', 'semester', 'week', 'Residence_week.json');
-            } else if (urlArr[2] == 'week'){ //주말
-                var jsonPath = path.join(__dirname, '..', 'timetable', 'semester', 'weekend', 'Residence_weekend.json');
+        case 'semester': // 학기중
+            if(urlArr[2] == 'week') { // 주중
+                var jsonPath = path.join(__dirname, '..', 'timetable', 'semester', 'week', 'Residence_week.json')
+            } else if (urlArr[2] == 'week') { // 주말
+                var jsonPath = path.join(__dirname, '..', 'timetable', 'semester', 'weekend', 'Residence_weekend.json')
             }
-            break;
-        case 'vacation': //방학중
-            if (urlArr[2] == 'week') { //주중
-                var jsonPath = path.join(__dirname, '..', 'timetable', 'vacation', 'week', 'Residence_week.json');
-            } else if (urlArr[2] == 'week') { //주말
-                var jsonPath = path.join(__dirname, '..', 'timetable', 'vacation', 'weekend', 'Residence_weekend.json');
+            break
+        case 'vacation': // 방학중
+            if (urlArr[2] == 'week') { // 주중
+                var jsonPath = path.join(__dirname, '..', 'timetable', 'vacation', 'week', 'Residence_week.json')
+            } else if (urlArr[2] == 'week') { // 주말
+                var jsonPath = path.join(__dirname, '..', 'timetable', 'vacation', 'weekend', 'Residence_weekend.json')
             }
         default:
-            break;
+            break
     }
-
 
     try {
-        var data = fs.readFileSync(jsonPath, 'UTF-8');
+        var data = fs.readFileSync(jsonPath, 'UTF-8')
     }
     catch (err) {
-        console.log(err);
-        return res.status(404).json({ error: 'Incorrect column' });
+        console.log(err)
+        return res.status(404).json({ error: 'Incorrect column' })
     }
-    const jsondata = JSON.parse(data);
-    return res.json(jsondata.residence_week);
-});
+    const jsondata = JSON.parse(data)
+    return res.json(jsondata.residence_week)
+})
 
-
-module.exports = router;
+module.exports = router
