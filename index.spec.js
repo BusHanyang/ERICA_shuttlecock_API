d44ef33d7e8a0a211bb66223eabe3ee1fcb3eafa
Yeonggyun Kim <kygha7205@naver.com>
2019-11-18 03:39:01 +0900

    Add Javascript Automating test script using mocha, chai and supertest.

--- index.spec.js
+++ index.spec.js
@@ -1,21 +1,35 @@
 const request = require('supertest');
 const { expect } = require("chai");
 const server = request.agent('http://localhost:3000');
+const { app } = require("./index");
+
+
+
 
 describe('GET /', () => {
-  it("should respond with json", done => {
+  it('should respond with status code of 200', (done) => {
     server
-
       .get("/semester/week/subway")
-      .expect(200)
+      .expect(200 || 201)
       .end((err, res) => {
         if (err) {
           done(err);
           return;
         }
-
-        expect("Content-Type", /json/);
         done();
       });
-  }).timeout(5000);
+  });
+
+    it("should respond with json", done => {
+      server
+        .get("/semester/week/subway")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
 });
