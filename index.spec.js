e8af180b8350d8ee61d23d2b5947bb4e2d5b0228
Yeonggyun Kim <kygha7205@naver.com>
2019-11-20 22:19:16 +0900

    Add testing commands

--- index.spec.js
+++ index.spec.js
@@ -7,10 +7,10 @@ const { app } = require("./index");
 
 
 describe('GET /', () => {
-  it('should respond with status code of 200', (done) => {
+  it('index page should respond with status code of 404', (done) => {
     server
-      .get("/semester/week/subway")
-      .expect(200 || 201)
+      .get("/")
+      .expect(404)
       .end((err, res) => {
         if (err) {
           done(err);
@@ -20,9 +20,126 @@ describe('GET /', () => {
       });
   });
 
-    it("should respond with json", done => {
+    it("should respond with json - /semester/week/subway", done => {
       server
         .get("/semester/week/subway")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /semester/week/shuttlecock_i", done => {
+      server
+        .get("/semester/week/shuttlecock_i")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /semester/week/shuttlecock_o", done => {
+      server
+        .get("/semester/week/shuttlecock_o")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /semester/week/giksa", done => {
+      server
+        .get("/semester/week/giksa")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /semester/week/yesulin", done => {
+      server
+        .get("/semester/week/yesulin")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /vacation/week/subway", done => {
+      server
+        .get("/semester/week/subway")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /vacation/week/shuttlecock_i", done => {
+      server
+        .get("/semester/week/shuttlecock_i")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /vacation/week/shuttlecock_o", done => {
+      server
+        .get("/semester/week/shuttlecock_o")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /vacation/week/giksa", done => {
+      server
+        .get("/semester/week/giksa")
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          done();
+        });
+    });
+
+    it("should respond with json - /vacation/week/yesulin", done => {
+      server
+        .get("/semester/week/yesulin")
         .expect("Content-Type", /json/)
         .end((err, res) => {
           if (err) {
