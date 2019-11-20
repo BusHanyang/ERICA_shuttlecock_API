79ab4d31787d310425cc27115da1b2f24e192638
Yeonggyun Kim <kygha7205@naver.com>
2019-11-21 03:50:29 +0900

    Delete useless routing module.

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const express = require("express");
 const moment = require("moment");
-const https = require('https')
+const https = require("https");
 const fs = require("fs");
 const http = require("http");
 // const port = 3000;
 const app = express();
 
 //FOR GITHUB ACTIONS CI TEST
-if (process.env.cert_secret){
+if (process.env.cert_secret) {
   var options = {
     key: process.env.priv,
     cert: process.env.cert
   };
 
-//IF RUN IN RELEASE 
+  //IF RUN IN RELEASE
 } else {
   /*https*/
   var options = {
-    key: fs.readFileSync('ssl/privkey.pem'),
-    cert: fs.readFileSync('ssl/cert.pem')
+    key: fs.readFileSync("ssl/privkey.pem"),
+    cert: fs.readFileSync("ssl/cert.pem")
   };
-
 }
-
 
 /*load setting value from json file*/
 const data = JSON.parse(fs.readFileSync("settings.json", "UTF-8"));
 const now = moment().format("YYYY-MM-DD");
-
 
 /*Functions about define datekind and daykind to make URL query string. */
 function getKind() {
@@ -59,11 +56,8 @@ function getDayKind() {
   }
 }
 
-
 const daykind = getDayKind();
 const datekind = getKind();
-
-
 /*middle ware*/
 app.use(express.static(__dirname + "/public"));
 app.use(function(req, res, next) {
@@ -73,147 +67,19 @@ app.use(function(req, res, next) {
   next();
 });
 
-
 /* router */
-
-/*학기중*/
+app.use("/" + datekind + "/" + daykind + "/giksa", require("./router/giksa"));
 app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa"
-  ],
-  require("./router/giksa")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
-  ],
+  "/" + datekind + "/" + daykind + "/shuttlecock_i",
   require("./router/shuttlecock_i")
 );
 app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o"
-  ],
+  "/" + datekind + "/" + daykind + "/shuttlecock_o",
   require("./router/shuttlecock_o")
 );
+app.use("/" + datekind + "/" + daykind + "/subway", require("./router/subway"));
 app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway"
-  ],
-  require("./router/subway")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin"
-  ],
-  require("./router/yesulin")
-);
-
-/*방학중 - 계절학기*/
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa"
-  ],
-  require("./router/giksa")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
-  ],
-  require("./router/shuttlecock_i")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_o"
-  ],
-  require("./router/shuttlecock_o")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway"
-  ],
-  require("./router/subway")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin"
-  ],
-  require("./router/yesulin")
-);
-
-/*방학중*/
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa",
-    "/" + datekind + "/" +  daykind + "/giksa"
-  ],
-  require("./router/giksa")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
-    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
-  ],
-  require("./router/shuttlecock_i")
-);
-app.use(
-  [
-    "/" + datekind + "/" + daykind + "/shuttlecock_o",
-    "/" + datekind + "/" + daykind + "/shuttlecock_o",
-    "/" + datekind + "/" + daykind + "/shuttlecock_o",
-    "/" + datekind + "/" + daykind + "/shuttlecock_o"
-  ],
-  require("./router/shuttlecock_o")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway",
-    "/" + datekind + "/" +  daykind + "/subway"
-  ],
-  require("./router/subway")
-);
-app.use(
-  [
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin",
-    "/" + datekind + "/" +  daykind + "/yesulin"
-  ],
+  "/" + datekind + "/" + daykind + "/yesulin",
   require("./router/yesulin")
 );
 
