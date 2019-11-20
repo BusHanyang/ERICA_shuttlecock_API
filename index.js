2d63faacce29007dc754d22c57b568592b93acbd
Yeonggyun Kim <kygha7205@naver.com>
2019-11-21 03:21:42 +0900

    Change URI query string define method, using moment.js.

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const moment = require("moment");
 // const https = require('https')
-const fs = require('fs')
-const http = require('http')
+const fs = require("fs");
+const http = require("http");
 // const port = 3000;
 const app = express();
 
+/*load setting value from json file*/
+const data = JSON.parse(fs.readFileSync("settings.json", "UTF-8"));
+const now = moment().format("YYYY-MM-DD");
 
+
+/*Functions about define datekind and daykind to make URL query string. */
+function getKind() {
+  const semester = data.calendar[0].semester;
+  const vacation_session = data.calendar[1].vacation_session;
+  const vacation = data.calendar[2].vacation;
+
+  if (moment(now).isBetween(semester.start, semester.end)) {
+    return "semester";
+  } else if (
+    moment(now).isBetween(vacation_session.start, vacation_session.end)
+  ) {
+    return "vacation_session";
+  } else if (moment(now).isBetween(vacation.start, vacation.end)) {
+    return "vacation";
+  } else {
+    return "error";
+  }
+}
+
+function getDayKind() {
+  switch (moment().format("dddd")) {
+    case "Sunday":
+      return "weekend";
+    case "Saturday":
+      return "weekend";
+    default:
+      return "week";
+  }
+}
+
+
+const daykind = getDayKind();
+const datekind = getKind();
 
 /*https*/
 //Delete Https connection info for OSS class.
@@ -18,54 +55,167 @@ const app = express();
 //     cert: fs.readFileSync('ssl/cert.pem')
 // };
 
-
-
 /*middle ware*/
-app.use(express.static(__dirname + '/public'));
-app.use(function (req, res, next){
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Methods", 'GET');//POST, PUT, DELETE 는 구현하지 않음.
-    res.header("Access-Control-Allow-Headers", "content-type");
-    next();
-})
+app.use(express.static(__dirname + "/public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET"); //POST, PUT, DELETE 는 구현하지 않음.
+  res.header("Access-Control-Allow-Headers", "content-type");
+  next();
+});
 
-/* body-parser */
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
 
 /* router */
 
 /*학기중*/
-app.use(['/semester/week/giksa', '/semester/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
-app.use(['/semester/week/shuttlecock_i', '/semester/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/semester/week/shuttlecock_o', '/semester/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/semester/week/subway', '/semester/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
-app.use(['/semester/week/yesulin', '/semester/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa"
+  ],
+  require("./router/giksa")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
+  ],
+  require("./router/shuttlecock_i")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o"
+  ],
+  require("./router/shuttlecock_o")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway"
+  ],
+  require("./router/subway")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin"
+  ],
+  require("./router/yesulin")
+);
 
 /*방학중 - 계절학기*/
-app.use(['/vacation_session/week/giksa', '/vacation_session/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
-app.use(['/vacation_session/week/shuttlecock_i', '/vacation_session/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/vacation_session/week/shuttlecock_o', '/vacation_session/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/vacation_session/week/subway', '/vacation_session/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
-app.use(['/vacation_session/week/yesulin', '/vacation_session/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
-
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa"
+  ],
+  require("./router/giksa")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
+  ],
+  require("./router/shuttlecock_i")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_o"
+  ],
+  require("./router/shuttlecock_o")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway"
+  ],
+  require("./router/subway")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin"
+  ],
+  require("./router/yesulin")
+);
 
 /*방학중*/
-app.use(['/vacation/week/giksa', '/vacation/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
-app.use(['/vacation/week/shuttlecock_i', '/vacation/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/vacation/week/shuttlecock_o', '/vacation/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/vacation/week/subway', '/vacation/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
-app.use(['/vacation/week/yesulin', '/vacation/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
-
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa",
+    "/" + datekind + "/" +  daykind + "/giksa"
+  ],
+  require("./router/giksa")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i",
+    "/" + datekind + "/" +  daykind + "/shuttlecock_i"
+  ],
+  require("./router/shuttlecock_i")
+);
+app.use(
+  [
+    "/" + datekind + "/" + daykind + "/shuttlecock_o",
+    "/" + datekind + "/" + daykind + "/shuttlecock_o",
+    "/" + datekind + "/" + daykind + "/shuttlecock_o",
+    "/" + datekind + "/" + daykind + "/shuttlecock_o"
+  ],
+  require("./router/shuttlecock_o")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway",
+    "/" + datekind + "/" +  daykind + "/subway"
+  ],
+  require("./router/subway")
+);
+app.use(
+  [
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin",
+    "/" + datekind + "/" +  daykind + "/yesulin"
+  ],
+  require("./router/yesulin")
+);
 
 app.use((req, res, next) => {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 app.use((err, req, res, next) => {
-  res.status(404)
-  res.json({ errorcode: '404' })
-})
+  res.status(404);
+  res.json({ errorcode: "404" });
+});
 
 /*server*/
 // Create an HTTP service.
@@ -73,4 +223,4 @@ http.createServer(app).listen(3000);
 // Create an HTTPS service identical to the HTTP service.
 // https.createServer(options, app).listen(443);
 
-module.exports = app
+module.exports = app;
