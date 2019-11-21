0ae0306e4d24adde8176afa55234e959d0cdbf7c
Yeonggyun Kim <kygha7205@naver.com>
2019-11-21 19:13:57 +0900

    Roll back routing modules to access with full URI query string.

--- index.js
+++ index.js
@@ -34,26 +34,27 @@ app.use(function(req, res, next) {
 });
 
 /* router */
-app.use(
-  ["/" + datekind + "/" + daykind + "/giksa", "/giksa"],
-  require("./router/giksa")
-);
-app.use(
-  ["/" + datekind + "/" + daykind + "/shuttlecock_i", "/shuttlecock_i"],
-  require("./router/shuttlecock_i")
-);
-app.use(
-  ["/" + datekind + "/" + daykind + "/shuttlecock_o", "/shuttlecock_o"],
-  require("./router/shuttlecock_o")
-);
-app.use(
-  ["/" + datekind + "/" + daykind + "/subway", "/subway"],
-  require("./router/subway")
-);
-app.use(
-  ["/" + datekind + "/" + daykind + "/yesulin", "/yesulin"],
-  require("./router/yesulin")
-);
+/*학기중*/
+app.use(['/semester/week/giksa', '/semester/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa', '/giksa'], require('./router/giksa'))
+app.use(['/semester/week/shuttlecock_i', '/semester/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i', '/shuttlecock_i'], require('./router/shuttlecock_i'))
+app.use(['/semester/week/shuttlecock_o', '/semester/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o', '/shuttlecock_o'], require('./router/shuttlecock_o'))
+app.use(['/semester/week/subway', '/semester/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway', '/subway'], require('./router/subway'))
+app.use(['/semester/week/yesulin', '/semester/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin', '/yesulin'], require('./router/yesulin'))
+
+/*방학중 - 계절학기*/
+app.use(['/vacation_session/week/giksa', '/vacation_session/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
+app.use(['/vacation_session/week/shuttlecock_i', '/vacation_session/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
+app.use(['/vacation_session/week/shuttlecock_o', '/vacation_session/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
+app.use(['/vacation_session/week/subway', '/vacation_session/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
+app.use(['/vacation_session/week/yesulin', '/vacation_session/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
+
+
+/*방학중*/
+app.use(['/vacation/week/giksa', '/vacation/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
+app.use(['/vacation/week/shuttlecock_i', '/vacation/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
+app.use(['/vacation/week/shuttlecock_o', '/vacation/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
+app.use(['/vacation/week/subway', '/vacation/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
+app.use(['/vacation/week/yesulin', '/vacation/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
 
 app.use((req, res, next) => {
   next(createError(404));
