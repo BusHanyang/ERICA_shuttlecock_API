2d25bca5a7cf39f2719d78f608a14ca5694244f5
Yeonggyun Kim <kygha7205@naver.com>
2019-12-22 23:15:43 +0900

    Change mocha test script running port to 8080

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const { expect } = require("chai");
-const server = request.agent('http://localhost:3000');
+const server = request.agent('http://localhost:8080');
 const { app } = require("./index");
 
 
