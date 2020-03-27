5d6bc2c3c23301884ef5d123c28419c659644e98
HyeongChang Lee <hc9904@hanyang.ac.kr>
2020-03-27 23:27:13 +0900

    Update logic of api

--- common.js
+++ common.js
@@ -1,10 +1,11 @@
 const moment = require("moment");
 const fs = require("fs");
 
-
 /*load setting value from json file*/
 const data = JSON.parse(fs.readFileSync("./settings.json", "UTF-8"));
-const now = moment().utcOffset('+0900').format("YYYY-MM-DD");
+const now = moment()
+  .utcOffset("+0900")
+  .format("YYYY-MM-DD");
 
 /*Functions about define datekind and daykind to make URL query string. */
 function getDateKind() {
@@ -15,7 +16,12 @@ function getDateKind() {
   if (moment(now).isBetween(semester.start, semester.end, null, [])) {
     return "semester";
   } else if (
-    moment(now).isBetween(vacation_session.start, vacation_session.end, null, [])
+    moment(now).isBetween(
+      vacation_session.start,
+      vacation_session.end,
+      null,
+      []
+    )
   ) {
     return "vacation_session";
   } else if (moment(now).isBetween(vacation.start, vacation.end, null, [])) {
@@ -29,34 +35,35 @@ function getDayKind() {
   const holiday = data.calendar[3].holiday;
 
   // console.log((moment().utcOffset('+0900').day()));
-  let momentDay = moment().utcOffset('+0900').day();
-  if(holiday.indexOf(now) !== -1){//holiday 배열에서 오늘날짜 값을 찾을 경우
+  let momentDay = moment()
+    .utcOffset("+0900")
+    .day();
+  if (holiday.indexOf(now) !== -1) {
+    //holiday 배열에서 오늘날짜 값을 찾을 경우
     console.log("공휴일 보정");
-    momentDay = 0;//일요일로 날짜 보정.
+    momentDay = 0; //일요일로 날짜 보정.
   }
 
   switch (momentDay) {
-    case 0://sunday
+    case 0: //sunday
       return "weekend";
-    case 6://saturday
+    case 6: //saturday
       return "weekend";
     default:
       return "week";
   }
-  
 }
 
-function isHalt(){
-    const haltList = data.calendar[4].halt;
-    console.log(haltList);
-    if(haltList.indexOf(now) !== -1){
-      return "halted"
-    } else {
-      return ""
-    }
+function isHalt() {
+  const haltList = data.calendar[4].halt;
+  console.log(haltList);
+  if (haltList.indexOf(now) !== -1) {
+    return "halted";
+  } else {
+    return "";
+  }
 }
 
-
-module.exports.getDayKind = getDayKind
-module.exports.getDateKind = getDateKind
-module.exports.isHalt = isHalt
+module.exports.getDayKind = getDayKind;
+module.exports.getDateKind = getDateKind;
+module.exports.isHalt = isHalt;
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const http = require("http");
 const app = express();
-const helmet = require('helmet')
+const helmet = require("helmet");
 
-app.use(helmet())
-app.use(helmet.noCache())
+app.use(helmet());
+app.use(helmet.noCache());
 
 /*middle ware*/
 app.use(express.static(__dirname + "/public"));
@@ -16,40 +15,7 @@ app.use(function(req, res, next) {
 });
 
 /* router */
-/*학기중*/
-app.use(['/semester/week/giksa', '/semester/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa', '/giksa'], require('./router/giksa'))
-app.use(['/semester/week/shuttlecock_i', '/semester/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i', '/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/semester/week/shuttlecock_o', '/semester/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o', '/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/semester/week/subway', '/semester/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway', '/subway'], require('./router/subway'))
-app.use(['/semester/week/yesulin', '/semester/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin', '/yesulin'], require('./router/yesulin'))
-
-/*방학중 - 계절학기*/
-app.use(['/vacation_session/week/giksa', '/vacation_session/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
-app.use(['/vacation_session/week/shuttlecock_i', '/vacation_session/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/vacation_session/week/shuttlecock_o', '/vacation_session/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/vacation_session/week/subway', '/vacation_session/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
-app.use(['/vacation_session/week/yesulin', '/vacation_session/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
-
-
-/*방학중*/
-app.use(['/vacation/week/giksa', '/vacation/weekend/giksa', 'vacation/week/giksa', 'vacation/weekend/giksa'], require('./router/giksa'))
-app.use(['/vacation/week/shuttlecock_i', '/vacation/weekend/shuttlecock_i', 'vacation/week/shuttlecock_i', 'vacation/weekend/shuttlecock_i'], require('./router/shuttlecock_i'))
-app.use(['/vacation/week/shuttlecock_o', '/vacation/weekend/shuttlecock_o', 'vacation/week/shuttlecock_o', 'vacation/weekend/shuttlecock_o'], require('./router/shuttlecock_o'))
-app.use(['/vacation/week/subway', '/vacation/weekend/subway', 'vacation/week/subway', 'vacation/weekend/subway'], require('./router/subway'))
-app.use(['/vacation/week/yesulin', '/vacation/weekend/yesulin', 'vacation/week/yesulin', 'vacation/weekend/yesulin'], require('./router/yesulin'))
-
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-app.use((err, req, res, next) => {
-  res.status(404);
-  res.json({ errorcode: "404" });
-});
-
-/*server*/
-// Create an HTTP service.
-http.createServer(app).listen(8080);
-
+var routes = require("./router/routes");
+routes(app);
 
 module.exports = app;
