9cd8cecbffbd21adc497fc64d421cf2204565047
Yeonggyun.Kim <kygha7205@naver.com>
2020-04-22 03:39:46 +0900

    Fix testscript error due to modified server structure

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const { expect } = require("chai");
 const server = request.agent('http://localhost:8080');
-const { app } = require("./index");
+const { app } = require("./bin/www");
 
 
 
